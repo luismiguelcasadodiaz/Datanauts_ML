90bbde3b03580949956df8012e20ed7981be0b24
Luis Miguel Casado Diaz <luismiguelcasadodiaz@gmail.com>
2023-10-04 11:53:15 +0200

    substituion of Zeros by NaN

--- src/preprocess/satellite_preprocess.py
+++ src/preprocess/satellite_preprocess.py
@@ -12,9 +12,13 @@
 # **************************************************************************** #
 import pandas as pd
 import os
+from sklearn.preprocessing import RobustScaler
 
-
-
+def convert_zero_to_nan(value):
+	if value == 0:
+		return np.nan
+	else:
+		return value
 
 # File path
 os.chdir("../..")
@@ -25,12 +29,23 @@ sa_file_name = 'sa_2016_2023_raw.csv'
 ou_file_name = 'satellite_data.csv'
 sa_file_path = sa_data_folder + sa_file_name
 ou_file_path = sa_data_folder + ou_file_name
-sa = pd.read_csv(sa_file_path, delimiter = ',', parse_dates=[0],  header = None)
+#ingest data
+#sa = pd.read_csv(sa_file_path, delimiter = ',', parse_dates=[0],  header = None, converters={0:convert_zero_to_nan} )
+sa = pd.read_csv(sa_file_path, delimiter = ',', parse_dates=[0],  header = None, na_values=['0'])
+# set headers
 columns = ["date", "MF_nT_GSE_x",  "MF_nT_GSE_y", "MF_nT_GSE_z"]
 for i in range(4, 54):
 	columns.append(f"C_{i:02d}")
 sa.columns = columns
-sa.fillna(0, inplace=True)
 print(sa.iloc[[0,1, 2,sa.index[-3],sa.index[-2],sa.index[-1]]])
-sa.to_csv(ou_file_path, index=False)
+#Substitute NAN by 0
+sa.fillna(0, inplace=True)
+
+#Normaliza
+scaler = RobustScaler()
+sa = pd.DataFrame(scaler.fit_transform(sa), columns=sa.columns[1:])
+
+#save data
+print(sa.iloc[[0,1, 2,sa.index[-3],sa.index[-2],sa.index[-1]]])
+#sa.to_csv(ou_file_path, index=False)
 
